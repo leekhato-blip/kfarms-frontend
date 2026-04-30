b6d439969fbd1b7226a2962f1e4f8388662b9eeb
leekhato-blip <leekhato@gmail.com>
2026-04-30 17:11:42 +0100

    Show deployed frontend build metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import BuildInfoBadge from "./components/BuildInfoBadge";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <BuildInfoBadge />
+    </>
+  );
 }
